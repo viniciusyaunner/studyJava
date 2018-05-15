db3311a634576d22351fc52c39976416efdcc386
Vinicius Figueiredo <vinifysantos@gmail.com>
2018-05-15 09:51:57 -0300

    Add files via upload

---
+++ Perguntas/src/Perguntas/Pontos.java
@@ -0,0 +1,19 @@
+package Perguntas;
+
+public class Pontos {
+	private static int certo = 0;
+	private static int errado = 0;
+	public static int getCerto() {
+		return certo;
+	}
+	public static void setCerto(int certo) {
+		Pontos.certo = certo;
+	}
+	public static int getErrado() {
+		return errado;
+	}
+	public static void setErrado(int errado) {
+		Pontos.errado = errado;
+	}
+	
+}
