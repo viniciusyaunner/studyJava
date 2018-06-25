761d65cae892b651a6e38ec544b97867e9d4911d
ViniciusFigueiredo <ViniciusFigueiredo@github.com>
2018-06-25 18:22:55 -0300

    via commit

---
+++ posto de gasolina/pGasolina.sql
@@ -0,0 +1,33 @@
+﻿create database pGasolina;
+use pGasolina;
+
+create table combustivel(
+codcomb int not null,
+nome varchar(30),
+preco double,
+primary key(codcomb));
+
+create table fornecedor(
+codf int not null,
+nome varchar(20),
+diaentre date,
+primary key(codf));
+
+create table vendedor(
+codv int not null,
+nome varchar(20),
+expediente date,
+primary key(codv));
+
+create table cliente(
+codcli int not null,
+nome varchar(20),
+pagamento double,
+tipoPagamento varchar(20),
+primary key(codcli));
+
+insert into combustivel value
+(1,"diesel",4.00),
+(2,"etanol",4.00),
+(3,"gasolina",4.00);
+
---
+++ relojoaria/relojoaria.sql
@@ -0,0 +1,15 @@
+﻿create database relojoaria;
+use relojoaria;
+
+create table produtos(
+codpro int not null,
+nomep varchar(15),
+preco double,
+datap date,
+fornecedor varchar(15),
+primary key(codpro));
+
+insert into produtos value
+(4,"relogio de parede","50.00","2018-05-14","Jacob"),
+(5,"relogio de pulso","123.00","2018-05-14","Jacob"),
+(6,"relojio de plastico","23.00","2018-05-14","Jacob");
